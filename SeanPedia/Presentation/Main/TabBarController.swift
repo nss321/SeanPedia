c35cad7674b01ed7b9f15772aba2fd95587e78ab
BAE <lexon1209@gmail.com>
2025-01-27 23:33:09 +0900

    Feat: 탭바 추가

---
+++ SeanPedia/Presentation/Main/TabBarController.swift
@@ -0,0 +1,27 @@
+//
+//  TabBarController.swift
+//  SeanPedia
+//
+//  Created by BAE on 1/27/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configTabBar()
+        setupTabbarAppearance()
+    }
+    
+    func configTabBar() {
+    }
+
+    func setupTabbarAppearance() {
+    }
+}
+
